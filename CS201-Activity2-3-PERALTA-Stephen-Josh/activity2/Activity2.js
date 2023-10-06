0c58c05dc22fff8490a8780ed7f7b4e24a64f482
Stephenjoshperalta <147068479+Stephenjoshperalta@users.noreply.github.com>
2023-10-06 00:55:55 -0700

    Add files via upload

---
+++ CS201-Activity2-3-PERALTA-Stephen-Josh/activity2/Activity2.js
@@ -0,0 +1,12 @@
+let firstFavNum = 102;
+let secondFavNum = 234;
+console.log(firstFavNum>secondFavNum);
+console.log(firstFavNum<secondFavNum);
+console.log(firstFavNum>=secondFavNum);
+console.log(firstFavNum<=secondFavNum);
+console.log(firstFavNum === secondFavNum)
+console.log(firstFavNum == secondFavNum);
+console.log(firstFavNum !== secondFavNum);
+console.log(firstFavNum!=secondFavNum);
+
+
---
+++ CS201-Activity2-3-PERALTA-Stephen-Josh/activity3/activity3.js
@@ -0,0 +1,13 @@
+let password;
+
+
+if (password ===8){
+    console.log("welcome");
+}else if (password <= 8){
+    console.log("password too short");
+}else if (password >=8){
+    console.log ("too long password");
+    console.log("password should be 8 characters");
+}else{
+   console.log("please provide a password");
+}
